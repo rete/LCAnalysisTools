4a3a7cbbd3c9f129caa1cd52d413ab246daccdfe
Remi Ete <remi.ete@gmail.com>
2020-10-06 16:42:19 +0200

    Set warning as error in TravisCI

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -6,7 +6,7 @@ source $ILCSOFT/init_ilcsoft.sh
 cd /Package
 mkdir build
 cd build
-cmake -GNinja -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" .. && \
+cmake -GNinja -DCMAKE_CXX_FLAGS="-Werror -fdiagnostics-color=always" .. && \
 ninja  -k 0 && \
 ninja install 
 #&& \
